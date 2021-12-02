10f1f52e2c277e0d3a6a4e9c9744ebf49ec66cc1
John Bara <76656855+jbara2002@users.noreply.github.com>
2021-12-02 14:29:16 +0200

    Add files via upload

---
+++ Google Chrome.bat
@@ -0,0 +1,7 @@
+choco install googlechrome -y
+choco install chrome-remote-desktop-chrome -y
+choco install google-translate-chrome -y
+choco install google-cast-chrome -y
+choco install bitwarden-chrome -y
+choco install ublockorigin-chrome -y
+choco install bitwarden-edge -y
