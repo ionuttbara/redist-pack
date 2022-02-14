d12f3b76dbb7a5ec969b1151dd4bc1709ef6d099
geoni <76656855+jbara2002@users.noreply.github.com>
2022-02-14 19:26:59 +0200

    Delete Google Chrome.bat

--- Google Chrome.bat
+++
@@ -1,7 +0,0 @@
-choco install googlechrome -y
-choco install chrome-remote-desktop-chrome -y
-choco install google-translate-chrome -y
-choco install google-cast-chrome -y
-choco install bitwarden-chrome -y
-choco install ublockorigin-chrome -y
-choco install bitwarden-edge -y
